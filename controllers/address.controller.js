4c7d8584ddbac2b1fbae958ee0e9d73b803a2d34
Keny Travanino <keny03@unitec.edu>
2025-03-17 23:06:01 -0600

    modified: service for saving address and new servicio for getting address by job id

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -3,12 +3,14 @@ const Helper = require('../helpers/index');
 
 exports.saveAddress = async (req, res) => {
   try {
-    const { userId, cityId, neighborhood, directions } = req.body;
-    const addressFound = await Address.findOne({userId: userId}).populate('cityId');
+    const { userId, cityId, neighborhood, directions, jobId } = req.body;
+    const addressFound = Helper.isNullOrWhiteSpace(jobId) 
+      ? await Address.findOne({userId: userId}).populate('cityId')
+      : await Address.findOne({ jobId: jobId }).populate('cityId');
     
     if(Helper.isNullOrUndefined(addressFound)){
       // create address
-      const address = new Address({ cityId, neighborhood, directions, userId })
+      const address = new Address({ cityId, neighborhood, directions, userId, jobId })
       const addressCreated = await (await address.save()).populate('cityId');
       const addressResponse = {...addressCreated._doc};
       res.status(200).json({ message: 'Address created', data: addressResponse });
@@ -18,6 +20,7 @@ exports.saveAddress = async (req, res) => {
       const { _id } = req.body;
           let addressToUpdate = {
             ...( !Helper.isNullOrWhiteSpace(userId) && { userId } ),
+            ...( !Helper.isNullOrWhiteSpace(jobId) && { jobId } ),
             ...( !Helper.isNullOrWhiteSpace(cityId) && { cityId } ),
             ...( !Helper.isNullOrWhiteSpace(neighborhood) && { neighborhood } ),
             ...( !Helper.isNullOrWhiteSpace(directions) && { directions } ),
@@ -42,6 +45,21 @@ exports.getAddressByUserId = async (req, res) => {
   try {
     const { userId } = req.query;
     const address = await Address.findOne({userId: userId})
+      .populate('cityId')
+      .lean();
+    res.status(200).json({ message: 'Success getting address', data: address });
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({
+      message: error.message || 'Something goes wrong creating the user',
+    });
+  }
+};
+
+exports.getAddressByJobId = async (req, res) => {
+  try {
+    const { jobId } = req.query;
+    const address = await Address.findOne({jobId: jobId})
       .populate('cityId')
       .lean();
     res.status(200).json({ message: 'Success getting address', data: address });
